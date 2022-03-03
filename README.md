abf99d547b975ed39f7bbc08e5d61b89acbc7c63
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-03-03 14:10:58 -0500

    Triangle Divisor Problem update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WeeklyCodingChallenge
+
+` first triangle number for given divisor` Week of :  Feb 28 - Mar 4
+Not optimized
+     runs real slow but works ran til 200 and was pretty faster
+     anything after is delayed. My computer 500 was a few minutes
+
+
---
+++ src/com/revature/app/app.java
@@ -0,0 +1,27 @@
+package com.revature.app;
+
+import java.util.Scanner;
+
+import com.revature.problem.triNum;
+
+public class app {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Divisor input: ");
+		int num = scan.nextInt();
+		
+		triNum triNumber = new triNum();
+		int result = 0;
+		int count = 1;
+
+		while (triNumber.divCalc(result) < num) {
+			result = result + count;
+			count++;
+		}
+
+		System.out.println("The 1st number with " + num + " divisors is: " + result);
+		scan.close();
+	}
+}
