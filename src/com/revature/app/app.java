b7481c489c5220dace1495f154ae29f7a3d293dd
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-03-08 12:07:31 -0500

    update with fibinacci under 4000000 prob

--- src/com/revature/app/app.java
+++ src/com/revature/app/app.java
@@ -2,26 +2,32 @@ package com.revature.app;
 
 import java.util.Scanner;
 
+import com.revature.problem.Fib;
 import com.revature.problem.triNum;
 
 public class app {
 
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Divisor input: ");
-		int num = scan.nextInt();
+//		Scanner scan = new Scanner(System.in);
+//		System.out.print("Divisor input: ");
+//		int num = scan.nextInt();
+//		
+//		triNum triNumber = new triNum();
+//		int result = 0;
+//		int count = 1;
+//
+//		while (triNumber.divCalc(result) < num) {
+//			result = result + count;
+//			count++;
+//		}
+//
+//		System.out.println("The 1st number with " + num + " divisors is: " + result);
+//		scan.close();
+//	}
 		
-		triNum triNumber = new triNum();
-		int result = 0;
-		int count = 1;
-
-		while (triNumber.divCalc(result) < num) {
-			result = result + count;
-			count++;
-		}
-
-		System.out.println("The 1st number with " + num + " divisors is: " + result);
-		scan.close();
-	}
+		Fib fb = new Fib();
+		fb.calc();
+}
+	
 }
